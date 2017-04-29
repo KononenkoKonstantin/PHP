8c61d906fb5b20df8af43eadf3abb55205472caa
KononenkoKonstantin <kon-kos1@yandex.ua>
2017-04-29 23:23:36 +0300

    Create index.php

---
+++ Lesson4/mvc/index.php
@@ -0,0 +1,34 @@
+<?php
+    function __autoload($className){
+        $path = str_replace('\\',DIRECTORY_SEPARATOR, $className).'.class.php';
+        require_once $path;
+    }
+
+    $route = $_GET['r'];
+    $routeData = explode('/', $route);
+
+    if(empty($routeData[0])){
+        $routeData[0] = 'home';
+    }
+    if(empty($routeData[1])){
+    $routeData[1] = 'index';
+    }
+
+    $controllerName = '\core\controllers\\'.ucfirst($routeData[0]);
+    $actionName = 'action'.ucfirst($routeData[1]);
+    $path = substr(str_replace('\\',DIRECTORY_SEPARATOR, $controllerName).'.class.php', 1);
+
+    if(file_exists($path)){
+        $controller = new $controllerName;
+        if(method_exists($controller, $actionName)){
+            echo $controller->$actionName();
+        }
+        else{
+            header("HTTP/1.1 404 Not Found");
+            echo "Page not found!";
+        }
+    }else{
+        header("HTTP/1.1 404 Not Found");
+        echo "Page not found!";
+    }
+
